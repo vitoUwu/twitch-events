d2174a48128b063d5a0a8bf22f9373968ba5249b
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2022-08-07 21:57:04 -0400

    commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require("dotenv/config");
 const crypto = require('crypto')
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
     
 // Notification request headers
 const TWITCH_MESSAGE_ID = 'Twitch-Eventsub-Message-Id'.toLowerCase();
